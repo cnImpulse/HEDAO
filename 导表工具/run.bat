76d6f7b92b154826273245ee16634c612a24a9aa
impulse <2535189010@qq.com>
2022-07-07 19:47:44 +0800

    【导表】mac环境导表命令

--- 导表工具/run.bat
+++ 导表工具/run.bat
@@ -1,9 +1,9 @@
 dotnet Luban.ClientServer/Luban.ClientServer.dll -j cfg --^
- -d Defines/__root__.xml ^
- --input_data_dir Excels ^
- --output_code_dir ..\HEDAO\Assets\GameMain\Scripts\Cfg\Gen ^
- --output_data_dir ..\HEDAO\Assets\GameMain\Cfg\Bytes ^
- --gen_types code_cs_unity_bin,data_bin ^
- -s all 
+    -d Defines/__root__.xml ^
+    --input_data_dir Excels ^
+    --output_code_dir ..\HEDAO\Assets\GameMain\Scripts\Cfg\Gen ^
+    --output_data_dir ..\HEDAO\Assets\GameMain\Cfg\Bytes ^
+    --gen_types code_cs_unity_bin,data_bin ^
+    -s all 
 
 pause
---
+++ 导表工具/run.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+cd $(dirname $0)
+
+dotnet Luban.ClientServer/Luban.ClientServer.dll -j cfg -- \
+    -d Defines/__root__.xml \
+    --input_data_dir Excels \
+    --output_code_dir ../HEDAO/Assets/GameMain/Scripts/Cfg/Gen \
+    --output_data_dir ../HEDAO/Assets/GameMain/Cfg/Bytes \
+    --gen_types code_cs_unity_bin,data_bin \
+    -s all \
