2bd5a222f16b84a7fe8181c6f54ad28dc6668107
impulse <2535189010@qq.com>
2024-06-11 11:19:19 +0800

    【战斗】数值调整，Bug修复

--- HEDAO/Assets/GameMain/Scripts/Battle/Skill/Effect/AttackEffect.cs
+++ HEDAO/Assets/GameMain/Scripts/Battle/Skill/Effect/AttackEffect.cs
@@ -1,5 +1,6 @@
 using HEDAO;
 using UnityEngine;
+using UnityGameFramework.Runtime;
 
 namespace Cfg.Battle
 {
@@ -12,11 +13,11 @@ namespace Cfg.Battle
             var targetUnit = target as BattleUnit;
              if (DamageType == EDamageType.Physics)
             {
-                damage = Power + casterUnit?.Data?.RoleData?.STR ?? 0 - targetUnit.Data.RoleData.TPO;
+                damage = Power + (casterUnit?.Data?.RoleData?.STR ?? 0) - targetUnit.Data.RoleData.TPO;
             }
             else if (DamageType == EDamageType.Magic)
             {
-                damage = Power + casterUnit?.Data?.RoleData?.SSI ?? 0 - targetUnit.Data.RoleData.FAS;
+                damage = Power + (casterUnit?.Data?.RoleData?.SSI ?? 0) - targetUnit.Data.RoleData.FAS;
             }
             
             target.TakeDamage(Mathf.Max(0, damage));
